a1c130e53d212f4f0b681290a5c252b1fe108bf2
Sebsgabriel0604 <99741681+Sebsgabriel0604@users.noreply.github.com>
2022-02-27 08:54:51 -0600

    Add files via upload

---
+++ TareaNo.5_201800988/TareaNo.5_201800988/Module1.vb
@@ -0,0 +1,21 @@
+﻿Module Module1
+    Sub Main()
+        Dim num, x, i, m As Integer
+        x = 1
+        Console.WriteLine("Ingrese el numero que quiere factorial:")
+        num = Console.ReadLine()
+        If num > 0 Then
+            For y = 1 To num
+                x = x * y
+            Next
+        End If
+        Console.WriteLine("El factorial es del numero seleccionado es: " + x.ToString())
+        For i = 0 To x
+            If (i Mod 4) Then
+                m = m + 1
+            End If
+        Next
+        Console.WriteLine("Tiene " + m.ToString() + " numeros multiplos de 4 desde 0 hasta " + x.ToString())
+        Console.ReadLine()
+    End Sub
+End Module
